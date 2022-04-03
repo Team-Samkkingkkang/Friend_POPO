98d5673be891f90459066fc07237d116744a2a5a
gabang2 <offbeat1020@naver.com>
2022-04-03 10:56:15 +0900

    application added

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'popoapp',
+    'accountapp',
+    'boardapp',
+    'diaryapp',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [

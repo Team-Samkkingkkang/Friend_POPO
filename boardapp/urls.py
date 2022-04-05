0b4b0dd329acabfe62fa202f4d9e33d2fd146df0
gabang2 <offbeat1020@naver.com>
2022-04-05 22:51:10 +0900

    게시판 url연결 완료

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "boardapp"
 
+urlpatterns = [
+    path('', views.BoardMainView, name="BoardMainView")
+]
--- boardapp/views.py
+++ boardapp/views.py
@@ -1,2 +1,6 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
+
+
+def BoardMainView(request):
+    return render(request, template_name='boardapp/BoardMainTemplate.html')
